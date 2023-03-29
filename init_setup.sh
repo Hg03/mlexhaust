97fb15d3733a74abf94a609269c66810bc931ccb
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2023-03-29 17:25:01 +0000

    sh file updated

--- init_setup.sh
+++ init_setup.sh
@@ -1,8 +1,8 @@
 echo [$(date)]: "START"
 echo [$(date)]: "Creating conda env with python 3.8" # change py version as per your need
 conda create --prefix ./mlexhaust python=3.9 -y
-echo [$(date)]: "activate env"
-source activate ./env
+echo [$(date)]: "activate mlexhaust"
+source activate ./mlexhaust
 echo [$(date)]: "installing the requirements" 
 pip install -r requirements.txt
 echo [$(date)]: "END" 

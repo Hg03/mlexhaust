a2760723e68bd9c6a104a43ab250c8ece83ebc5a
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2023-03-31 21:30:03 +0530

    Delete init_setup.sh

--- init_setup.sh
+++
@@ -1,8 +0,0 @@
-echo [$(date)]: "START"
-echo [$(date)]: "Creating conda env with python 3.8" # change py version as per your need
-conda create --prefix ./mlexhaust python=3.9 -y
-echo [$(date)]: "activate mlexhaust"
-source activate ./mlexhaust
-echo [$(date)]: "installing the requirements" 
-pip install -r requirements.txt
-echo [$(date)]: "END" 

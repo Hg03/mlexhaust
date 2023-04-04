8adc86470a7d3e238e050ad92ab6c0b6beb60f08
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2023-04-04 19:36:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,31 +45,31 @@ def openai_():
 	submit = st.button('Submit')
 	
     try:	
-	    if submit is None:
-		openai.api_key = st.secrets['OPENAI_API_KEY']
-		mdlit("> ### Let's [red]first[/red] look at the Dalle API to generate some awesome images")
-		input_text_for_image = st.text_input("Put your imagination here to generate image")
-		if input_text_for_image:
-		    if st.button('Generate Image'):
-			url_img = generate_img(input_text_for_image)
-			st.info(f"View your awesome image at {url_img}")
-		mdlit("> ### Let's [blue]Secondly[/blue] look at ChatGPT api through which we can start a conversation")
-		input_text = st.text_input("You: ","Hi, What you need to talk about?", key="input")
-		llm = OpenAI(temperature=0.7,model_name="text-davinci-003")
+	if submit is None:
+            openai.api_key = st.secrets['OPENAI_API_KEY']
+	    mdlit("> ### Let's [red]first[/red] look at the Dalle API to generate some awesome images")
+	    input_text_for_image = st.text_input("Put your imagination here to generate image")
+	    if input_text_for_image:
+		if st.button('Generate Image'):
+		    url_img = generate_img(input_text_for_image)
+		    st.info(f"View your awesome image at {url_img}")
+		    mdlit("> ### Let's [blue]Secondly[/blue] look at ChatGPT api through which we can start a conversation")
+		    input_text = st.text_input("You: ","Hi, What you need to talk about?", key="input")
+		    llm = OpenAI(temperature=0.7,model_name="text-davinci-003")
 
-		if "entity_memory" not in st.session_state:
-		    st.session_state.entity_memory = ConversationEntityMemory(llm=llm,k=10)
+		    if "entity_memory" not in st.session_state:
+		        st.session_state.entity_memory = ConversationEntityMemory(llm=llm,k=10)
 
-		CONVERSATION = ConversationChain(llm=llm,prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,memory=st.session_state.entity_memory)
-		if input_text:
-		    output = CONVERSATION.run(input=input_text)
-		    st.session_state.past.append(input_text)
-		    st.session_state.generated.append(output)
+		    CONVERSATION = ConversationChain(llm=llm,prompt=ENTITY_MEMORY_CONVERSATION_TEMPLATE,memory=st.session_state.entity_memory)
+		    if input_text:
+		        output = CONVERSATION.run(input=input_text)
+		        st.session_state.past.append(input_text)
+		        st.session_state.generated.append(output)
 
-		if st.session_state['generated']:
-		    for i in range(len(st.session_state['generated'])-1, -1, -1):
-			message(st.session_state["generated"][i], key=str(i))
-			message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
+		    if st.session_state['generated']:
+		        for i in range(len(st.session_state['generated'])-1, -1, -1):
+			    message(st.session_state["generated"][i], key=str(i))
+			    message(st.session_state['past'][i], is_user=True, key=str(i) + '_user')
 
 	    else:
 		openai.api_key = user_api_key

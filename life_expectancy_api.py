674ee7060cbb47a56d179bc34513ca2d5a4fedf2
Harish Gehlot <69637720+Hg03@users.noreply.github.com>
2023-04-10 18:16:12 +0530

    Update life_expectancy_api.py

--- life_expectancy_api.py
+++ life_expectancy_api.py
@@ -52,4 +52,4 @@ def predict(data: Data):
     return {"prediction": prediction[0]}
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("life_expectancy_api:app", host="0.0.0.0", port=8000, reload=True)
